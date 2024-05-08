702be408e54781302b125b0d504b04b8f5270ff6
Hemanth Yadav K <109170547+Komarala@users.noreply.github.com>
2024-05-08 17:16:21 +0200

    Updated with random generator

--- mvMouse.py
+++ mvMouse.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import random
 
 # Define the time duration (in seconds)
 duration = 1000
@@ -7,11 +8,17 @@ duration = 1000
 # Get the current time
 start_time = time.time()
 
+# Define the range
+start = 1
+end = 1000
+
 # Start the loop
 while time.time() - start_time < duration:
     # Move the mouse pointer to a new position
-    pyautogui.moveTo(100, 100, duration=1)
-    pyautogui.moveTo(700, 700, duration=1)
+    pyautogui.moveTo(100, 100, duration=2)
+    # Generate a random number within the range
+    random_number = random.randint(start, end)
+    pyautogui.moveTo(random_number, random_number, duration=1)
 
     # Pause the execution for 1 second
-    time.sleep(1)
+    time.sleep(2)
